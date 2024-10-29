5a7704b8e3659e891a8978ff0deeaf2dae54da89
lucaspere <lucasrupestre@hotmail.com>
2024-10-29 10:50:19 -0300

    doc: add api/v1 to doc and routes

--- social/crates/social-service/src/apis/mod.rs
+++ social/crates/social-service/src/apis/mod.rs
@@ -31,18 +31,22 @@ pub fn setup_routes() -> Router<Arc<AppState>> {
         .routes(routes!(user_handlers::follow_user))
         .routes(routes!(user_handlers::unfollow_user));
 
-    let (user_router, user_openapi) = OpenApiRouter::with_openapi(api_doc)
-        .nest("/users", user_router)
-        .split_for_parts();
+    let user_router = OpenApiRouter::with_openapi(api_doc.clone()).nest("/users", user_router);
 
-    let (profile_router, profile_openapi) = OpenApiRouter::with_openapi(user_openapi)
-        .nest("/profiles", profile_router)
+    let profile_router =
+        OpenApiRouter::with_openapi(api_doc.clone()).nest("/profiles", profile_router);
+
+    let router = OpenApiRouter::new()
+        .merge(user_router)
+        .merge(profile_router);
+
+    let (api_router, api_openapi) = OpenApiRouter::new()
+        .nest("/api/v1", router)
         .split_for_parts();
 
     Router::new()
-        .merge(Scalar::with_url("/scalar", profile_openapi))
-        .merge(user_router)
-        .merge(profile_router)
+        .merge(Scalar::with_url("/docs", api_openapi))
+        .merge(api_router)
 }
 
 // fn setup_user_routes() {
