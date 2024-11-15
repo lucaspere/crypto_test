8ba1121bbb22b5b776f19c00ee12eb0db93f15dc
lucaspere <lucasrupestre@hotmail.com>
2024-11-15 09:43:15 -0300

    refactor: format notification price

--- social/crates/social-service/src/events/handlers/mod.rs
+++ social/crates/social-service/src/events/handlers/mod.rs
@@ -55,12 +55,12 @@ pub fn format_number_with_dynamic_precision(
     let mut precision = min_precision;
     while precision <= max_precision {
         let formatted = format!("{:.1$}", num, precision as usize);
-        if formatted.parse::<f64>().unwrap_or(0.0) != 0.0 {
+        if formatted.parse::<f64>().unwrap_or(0.0) == 0.0 {
             return formatted;
         }
         precision += 1;
     }
-    format!("{:.1$}", num, max_precision as usize)
+    format!("{:.1$}~", num, max_precision as usize)
 }
 
 pub fn format_time_elapsed(call_time: DateTime<FixedOffset>) -> String {
